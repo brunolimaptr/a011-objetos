193e6bf221eb2bb12913dd5a9b4c29c299b19c7c
brunolimaptr <brunolima.ptr@gmail.com>
2022-09-05 12:17:25 -0300

    projeto1

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+const arraysacolao = []
+
+const banana = {nome: "banana", preço: 10.20, disponibilidade: true}
+
+const uva = {nome: "uva", preço: 5.30, disponibilidade: true}
+
+const mamao = {nome: "mamão", preço: 7.90, disponibilidade: false}
+
+arraysacolao.push(banana,uva,mamao)
+
+console.log(arraysacolao)
